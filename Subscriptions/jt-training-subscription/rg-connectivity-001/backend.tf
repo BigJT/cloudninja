4e674499d02716d292d114dbb5fc2edb3fb92e61
BigJT <johntrihy@gmail.com>
2024-09-03 11:42:45 +0100

    first rg

---
+++ Subscriptions/jt-training-subscription/rg-connectivity-001/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name     = "tfstate-uksouth-rg"
+    storage_account_name    = "terraformbackendjtr01"
+    container_name          = "tfstatefiles"
+    key                     = "GitHub-Terraform-rg-connectivity-001"
+  }
+}
+provider "azurerm" {
+  features {}
+}
---
+++ Subscriptions/jt-training-subscription/rg-connectivity-001/variables.tf
@@ -0,0 +1,9 @@
+variable "Location" {
+    type        = string
+    default     = "uksouth"  
+}
+
+variable "ResourceGroup" {
+    type        = string
+    default     = "rg-connectivity-network-001"   
+}
