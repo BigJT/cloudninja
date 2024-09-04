9d74b6279a49769ea817b555e52ea3778607e5dc
BigJT <johntrihy@gmail.com>
2024-09-04 08:37:48 +0100

    fix terraform

--- Subscriptions/jt-training-subscription/rg-connectivity-001/main.tf
+++ Subscriptions/jt-training-subscription/rg-connectivity-001/main.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "resourcegroups" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet.vNetName
   address_space       = var.vnet.address_space
-  location            = azurerm_resource_group.resourcegroup.location
-  resource_group_name = azurerm_resource_group.resourcegroup.name
+  location            = azurerm_resource_group.resourcegroups.location
+  resource_group_name = azurerm_resource_group.resourcegroups.name
 
   
 }
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnets" {
   for_each = var.Subnets
   name                 = each.value["name"]
-  resource_group_name  = azurerm_resource_group.resourcegroup.name
+  resource_group_name  = azurerm_resource_group.resourcegroups.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = each.value["prefix"]
   
